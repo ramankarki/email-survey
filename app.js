1eb7c7554a98baacc57d8767efde234905da21bc
ramankarki <ramankarki40@gmail.com>
2021-01-14 12:57:21 +0545

    imported users route, initialized cookieSession, passport session middleware

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require("express");
-const googleOauth = require("./routes/google-oauth");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const googleOauth = require("./routes/googleOauth");
+const keys = require("./config/keys");
+const usersRoute = require("./routes/users");
 
 const app = express();
 
 // initialize global middleware
 app.use(express.json());
 
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 // google oauth
 app.use("/auth/google", googleOauth);
+app.use("/api/v1/users", usersRoute);
 
 module.exports = app;
