2bed2b952b7425d35566a4beb5c1114ae995bc17
ramankarki <ramankarki40@gmail.com>
2021-01-15 09:51:04 +0545

    project init

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,12 @@ import React from "react";
 
 class App extends React.Component {
   render() {
-    return <div>hello world</div>;
+    return (
+      <div>
+        hello world
+        <a href="/auth/google">sign in with google</a>
+      </div>
+    );
   }
 }
 
