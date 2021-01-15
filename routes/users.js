8f67d3cec896de835ec0b6900a467575153ce1fe
ramankarki <ramankarki40@gmail.com>
2021-01-15 18:39:26 +0545

    returns statusCode on response

--- routes/users.js
+++ routes/users.js
@@ -6,11 +6,13 @@ router.route("/").get((req, res) => {
   if (req.user) {
     res.status(200).json({
       status: "logged in",
+      statusCode: 200,
       user: req.user,
     });
   } else {
-    res.status(400).json({
+    res.status(200).json({
       status: "logged out",
+      statusCode: 400,
     });
   }
 });
