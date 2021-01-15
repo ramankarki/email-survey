1782208e24073721d4634237ee194723bd798b12
ramankarki <ramankarki40@gmail.com>
2021-01-15 12:28:44 +0545

    imported routes component and asigned to separate Route component with path

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,23 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Header from "./Header";
+import Landing from "./Landing";
+import Dashboard from "./Dashboard";
+import SurveysNew from "./SurveysNew";
+import "./App.css";
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        hello world
-        <a href="/auth/google">sign in with google</a>
-      </div>
+      <section>
+        <BrowserRouter>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route exact path="/surveys/new" component={SurveysNew} />
+        </BrowserRouter>
+      </section>
     );
   }
 }
