925299fc5322c29bd12da78316e2a86801236802
ramankarki <ramankarki40@gmail.com>
2021-01-15 21:06:13 +0545

    added code by text in the footer

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -16,12 +16,30 @@ class App extends React.Component {
 
   render() {
     return (
-      <section className="container">
+      <section>
         <BrowserRouter>
           <Header />
           <Route exact path="/" component={Landing} />
           <Route exact path="/surveys" component={Dashboard} />
           <Route exact path="/surveys/new" component={SurveysNew} />
+          <p
+            style={{
+              textAlign: "center",
+              margin: "2rem auto",
+              marginTop: "3rem",
+              width: "90%",
+              fontSize: ".8rem",
+            }}
+          >
+            full stack app coded by{" "}
+            <a
+              href="https://github.com/ramankarki"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Raman karki
+            </a>
+          </p>
         </BrowserRouter>
       </section>
     );
