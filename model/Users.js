da08e0b73ce0553e0dd89689ce597a40e82c3dd4
ramankarki <ramankarki40@gmail.com>
2021-01-16 21:09:54 +0545

    added credits property to schema

--- model/Users.js
+++ model/Users.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
   name: String,
   email: String,
   googleID: String,
+  credits: { type: Number, default: 0 },
 });
 
 const User = mongoose.model("users", userSchema);
