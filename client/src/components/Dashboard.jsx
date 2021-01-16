f7e3fae8aa7b5685fca14313c8bdc9189bdebe20
ramankarki <ramankarki40@gmail.com>
2021-01-16 20:54:40 +0545

    wired redux store and stripeCover component with credits and add credits button

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import StripeCover from "./StripeCover";
+import { connect } from "react-redux";
 
 const Dashboard = (props) => {
-  return <section>Dashboard</section>;
+  return (
+    <section style={{ width: "90%", maxWidth: "800px", margin: "auto" }}>
+      <h1 style={{ fontSize: "2rem" }}>Dashboard</h1>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          marginTop: "2rem",
+        }}
+      >
+        <p style={{ fontSize: "1.3rem", color: "#39bdc8" }}>
+          Credits: {props.auth ? props.auth.user.credits : 0}
+        </p>
+        <StripeCover />
+      </div>
+    </section>
+  );
 };
 
-export default Dashboard;
+const mapStateToProps = (state) => {
+  console.log(state);
+  return { auth: state.auth };
+};
+
+export default connect(mapStateToProps)(Dashboard);
