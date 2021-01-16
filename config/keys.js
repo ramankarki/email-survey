2272b63ea67743ac73614ff3e32079a77a528e1d
ramankarki <ramankarki40@gmail.com>
2021-01-16 11:02:31 +0545

    configured stipe keys

--- config/keys.js
+++ config/keys.js
@@ -4,6 +4,8 @@ if (process.env.NODE_ENV === "production") {
     googleClientKey: process.env.GOOGLE_CLIENT_KEY,
     dbstring: process.env.DBSTRING,
     cookieKey: process.env.COOKIE_KEY,
+    stripePublicKey: process.env.STRIPE_PUBLIC_KEY,
+    stripePrivateKey: process.env.STRIPE_PRIVATE_KEY,
   };
 } else {
   module.exports = require("./dev");
