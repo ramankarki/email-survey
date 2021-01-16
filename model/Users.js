bbd842307618140112cbcc5a95ea8047000ae768
ramankarki <ramankarki40@gmail.com>
2021-01-17 00:23:50 +0545

    added photo property

--- model/Users.js
+++ model/Users.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   name: String,
   email: String,
+  photo: String,
   googleID: String,
   credits: { type: Number, default: 0 },
 });
